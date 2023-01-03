3584352765caddc493fdb8a5e1a8800184e88131
Ajay Chavan <ajayc9838@gmail.com>
2023-01-03 09:11:04 +0530

    completed

---
+++ src/main/java/com/niit/C13_s5_mc/C13S5McApplication.java
@@ -0,0 +1,13 @@
+package com.niit.C13_s5_mc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C13S5McApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C13S5McApplication.class, args);
+	}
+
+}
